a121b8094b697d26041dca03ec184a6592cc843d
Yamamotokazunori <gaogaigar673@gmail.com>
2021-03-23 09:30:26 +0000

    [Start]2-6

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -12,13 +12,23 @@ class TodolistsController < ApplicationController
     # 3.トップ画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-  
+
   def index
     @lists = List.all
   end
-  
+
   def show
     @list = List.find(params[:id])
+  end
+
+  def edit
+    @list = List.find(params[:id])
+  end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
  private
